6172a800eba66716202143ec24007a1b8dd96ba4
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-03-07 23:45:16 +0700

    add middleware for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,32 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const accessToken = req.cookies.get('access_token')?.value;
-  console.log(accessToken);
+// export function middleware(req: NextRequest) {
+//   const accessToken = req.cookies.get('access_token')?.value;
+//   console.log(accessToken);
 
-  if (req.nextUrl.pathname === '/sign-in') {
-    return NextResponse.next();
-  }
+//   if (req.nextUrl.pathname === '/sign-in') {
+//     return NextResponse.next();
+//   }
 
-  if (
-    req.nextUrl.pathname.startsWith('/_next/') || // Next.js assets
-    req.nextUrl.pathname.startsWith('/favicon.ico') || // Favicon
-    req.nextUrl.pathname.match(
-      /\.(png|jpg|jpeg|svg|gif|ico|webp|woff2|woff|ttf|otf|css|js|json)$/,
-    ) // Public assets
-  ) {
-    return NextResponse.next();
-  }
+//   if (
+//     req.nextUrl.pathname.startsWith('/_next/') || // Next.js assets
+//     req.nextUrl.pathname.startsWith('/favicon.ico') || // Favicon
+//     req.nextUrl.pathname.match(
+//       /\.(png|jpg|jpeg|svg|gif|ico|webp|woff2|woff|ttf|otf|css|js|json)$/,
+//     ) // Public assets
+//   ) {
+//     return NextResponse.next();
+//   }
 
-  if (!accessToken) {
-    return NextResponse.redirect(new URL('/sign-in', req.url));
-  }
+//   if (!accessToken) {
+//     return NextResponse.redirect(new URL('/sign-in', req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
-};
+// export const config = {
+//   matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+// };
+
+export function middleware() {}
